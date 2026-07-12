705543375221bfeca4a4e07ebf6286d801cdd5f8
Numan <iamnumanriaz@gmail.com>
2026-07-12 23:21:44 +0500

    recalling python concept

---
+++ Day_1.py
@@ -0,0 +1,18 @@
+Heros = ["thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"]
+for hero in Heros:
+    print(hero.title())
+    
+
+hears = ("thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther")
+for hear in hears:
+    print(hear.title())
+    
+heros = {"thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"}
+
+
+Person = {
+    "Username": "JohnDoe",
+    "Password": "123456",
+    "age": 30,
+    
+}
