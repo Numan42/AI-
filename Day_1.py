79026b4f3157ed2049b9c7bec329910479f41c03
Numan <iamnumanriaz@gmail.com>
2026-07-12 23:36:04 +0500

    recalling concept

--- Day_1.py
+++ Day_1.py
@@ -1,18 +1,33 @@
-Heros = ["thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"]
-for hero in Heros:
-    print(hero.title())
+# Heros = ["thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"]
+# for hero in Heros:
+#     print(hero.title())
     
 
-hears = ("thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther")
-for hear in hears:
-    print(hear.title())
+# hears = ("thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther")
+# for hear in hears:
+#     # print(hear)
     
-heros = {"thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"}
+# # heros = {"thor", "hulk", "batman", "superman", "ironman", "spiderman", "captain america", "black widow", "doctor strange", "black panther"}
 
 
-Person = {
-    "Username": "JohnDoe",
-    "Password": "123456",
-    "age": 30,
+# Person = {
+#     "Username": "JohnDoe",
+#     "Password": "123456",
+#     "age": 30,
+    
+# }
+
+car = {
+    "make": "Toyota",
+    "model": "Camry",
+    "year": 2020,
+    "color": ["red", "blue", "black"],
     
 }
+print(car["color"])
+print(car["make"])
+x = car.values()
+print(x)
+
+for val in car.values():
+    print(val)
