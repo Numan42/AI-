9374188b6ba605392ab6ca69ce697bb1babfe7a1
Numan <iamnumanriaz@gmail.com>
2026-07-13 23:43:37 +0500

    slove second question

--- Day_2.py
+++ Day_2.py
@@ -4,3 +4,14 @@ def reverse_string(str):
     for char in str:
         result = char + result
     return result
+
+# print(reverse_string("hello"))
+
+"Check for Palindrome"
+def is_Palindrome(s):
+    cleaned = ''.join(char.lower() for char in s if char.isalnum())
+    
+    return cleaned == cleaned[::-1]
+
+print(is_Palindrome("A man a plan a canal Panama"))
+# "solve two question today"
