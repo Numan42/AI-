d83db5175926bd1347e2c215c7981dc5be3fcfc5
Numan <iamnumanriaz@gmail.com>
2026-07-14 23:43:29 +0500

    2nd question solve

--- Day_3.py
+++ Day_3.py
@@ -15,3 +15,12 @@ def non_repeat_char(s):
 
 
 print(non_repeat_char("hello numan what is new"))
+
+
+# """Find missing number from 1 to n in O(n) time."""
+
+def find_missing(nums):
+    n = len(nums) + 1  # n-1 numbers given, find the nth
+    expected_sum = n * (n + 1) // 2  # Sum of 1 to n
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
