4e8143979a49a76d88e96c8beb0d681a4fb0524d
Numan <iamnumanriaz@gmail.com>
2026-07-16 23:47:38 +0500

    method practice

--- Day_4.py
+++ Day_4.py
@@ -15,5 +15,27 @@ class Person:
 
 person_1 = Person("numan","itsnuman@gmail.com",111111,"west london")
         
-person_1.greet()
-person_1.person_address()
+# person_1.greet()
+# person_1.person_address()
+
+
+class Song:
+    def __init__(self, name):
+        self.name = name
+        self.song = []
+    
+    def add_song(self, song):
+        self.song.append(song)
+    
+    def remove_song(self, song):
+        self.song.append(song)
+    
+    def show_songs(self):
+        print(f"platlist'{self.song}'")
+        for songs in self.song:
+            print(songs)
+
+my_playlist = Song("Favorites")
+my_playlist.add_song("Bohemian Rhapsody")
+my_playlist.add_song("Stairway to Heaven")
+my_playlist.show_songs()
