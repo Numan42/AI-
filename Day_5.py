830aa74e8a3ae36df70be1f467ccf1805197d00f
Numan <iamnumanriaz@gmail.com>
2026-07-23 23:51:53 +0500

    inheritance concept

--- Day_5.py
+++ Day_5.py
@@ -1,7 +1,22 @@
 class Student:
-    def __init__(self, name, id, grade):
+    def __init__(self, name, id, standard):
         self.name = name
         self.id = id
+        self.standard = standard
+
+
+class Grade(Student):
+    def __init__(self, name, id, standard, subject, grade, percentage):
+        super().__init__(name, id, standard)
+        self.subject = subject
         self.grade = grade
+        self.percentage = percentage
     
-    
+    def show_student_info(self):
+        print(f"Student: {self.name} | ID: {self.id} | Standard: {self.standard} | "
+              f"Subject: {self.subject} | Grade: {self.grade} | Percentage: {self.percentage}%")
+
+
+# Usage
+student = Grade("Numan", 101, "Grade 10", "Mathematics", "A", 92.5)
+student.show_student_info()
